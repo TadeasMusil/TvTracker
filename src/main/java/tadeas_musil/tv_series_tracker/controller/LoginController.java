83089a86342a4e79a37ad48c8373a3c5a7ac327f
Tadeas <musil.tadeas@gmail.com>
2019-10-24 22:27:35 +0200

    Formatting changes

--- src/main/java/tadeas_musil/tv_series_tracker/controller/LoginController.java
+++ src/main/java/tadeas_musil/tv_series_tracker/controller/LoginController.java
@@ -1,7 +1,7 @@
 package tadeas_musil.tv_series_tracker.controller;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class LoginController {
